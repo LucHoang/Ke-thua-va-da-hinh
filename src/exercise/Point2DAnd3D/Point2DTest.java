8849714348cf8bf72ad711eed4bea004de5ea29d
LucHoang <hoangsyluc@gmail.com>
2021-02-22 16:32:09 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ src/exercise/Point2DAnd3D/Point2DTest.java
@@ -0,0 +1,12 @@
+package exercise.Point2DAnd3D;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+
+        point2D = new Point2D(3,4);
+        System.out.println(point2D);
+    }
+
+}
